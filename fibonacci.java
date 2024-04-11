251fcd94410ea4dfc64713eaff45944d437d96ae
Eike-Mattos <161317519+Eike-Mattos@users.noreply.github.com>
2024-04-11 07:55:20 -0300

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -7,17 +7,17 @@ public class fibonacci {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("informe o valor do limite ");
+		System.out.println("insira o valor do limite ");
 		int limite = sc.nextInt();
-		int num1 = 0;
-		int num2 = 1;
+		int numero1 = 0;
+		int numero2 = 1;
 		int cont, res;
-		System.out.println(num2);
+		System.out.println(numero2);
 		for(cont = 2; cont<=limite; cont++) {
-			res = num1 + num2;
+			res = numero1 + numero2;
 			System.out.println(res);
-			num1 = num2;
-			num2 = res;
+			numero1 = numero2;
+			numero2 = res;
 		}
 
 	}
